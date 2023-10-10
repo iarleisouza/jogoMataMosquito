c1bf52228b22fc67cb59964d9624477262ca35b7
iarleisouza <65626531+iarleisouza@users.noreply.github.com>
2023-10-09 22:30:51 -0300

    Add files via upload

---
+++ jogo.js
@@ -0,0 +1,111 @@
+let altura = 0
+let largura = 0
+let vidas = 1
+let tempo = 15
+
+
+
+let criaMosquitoTempo = 1500
+
+let nivel = window.location.search
+//alert(nivel.replace('?', ''))
+ nivel = (nivel.replace('?', ''))
+
+if (nivel === 'normal') {
+    //1500
+    criaMosquitoTempo = 1500
+} else if (nivel === 'dificil'){
+ //1000
+     criaMosquitoTempo = 1000
+} else if (nivel === 'chucknorris') {
+    // 750
+    criaMosquitoTempo = 750
+}
+
+function ajustaTamanhoPalcoJogo(){
+    altura = window.innerHeight
+    largura = window.innerWidth
+    console.log(altura,largura)
+}
+ajustaTamanhoPalcoJogo()
+
+let cronometro = setInterval(function(){
+
+         tempo -= 1
+         if(tempo < 0){
+            clearInterval(cronometro)
+            clearInterval(criaMosquito)
+            window.location.href = 'vitoria.html'
+         }else {
+        document.getElementById('cronometro').innerHTML = tempo
+         }
+}, 1000)
+
+function posicaoRandomica(){
+
+//remover o mosquito anterior (caso exista)
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+
+          //console.log('elemento selecionado foi: v' + vidas)
+          if (vidas > 3)
+         // alert('Game Over')
+        window.location.href = 'fim_de_jogo.html'
+        document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+        vidas++
+    }
+    
+    let posicaoX = Math.floor(Math.random() * largura) - 90
+    let posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+    
+    //criar o elemento HTML
+    let mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
+
+    document.body.appendChild(mosquito)
+
+    tamanhoAleatorio()
+}
+
+
+function tamanhoAleatorio(){
+    let classe = Math.floor(Math.random() * 3)
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+        
+        case 1:
+            return 'mosquito2'
+
+        case 2:
+            return 'mosquito3'
+    }
+    console.log(classe)
+}
+function ladoAleatorio(){
+    let classe = Math.floor(Math.random() * 2)
+    switch (classe) {
+        case 0:
+            return 'ladoA'
+        
+        case 1:
+            return 'LadoB'
+
+       
+    }
+    console.log(classe)
+}
+
